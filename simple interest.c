3b4f70638b35f7bf7b0090fcf74dbb42016a349d
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:21:50 +0530

    Delete simple interest.c

--- simple interest.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int p,r,t,a;
-    printf("enter principle amount,rate,time");
-    scanf("%d%d%d",&p,&r,&t);
-    a = (p*r*t)/100;
-    printf("%d is the simple interest",a);
-}
