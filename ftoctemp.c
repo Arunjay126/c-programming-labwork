da49397e8561261b03a778904ebf767d3727a183
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:21:18 +0530

    Delete ftoctemp.c

--- ftoctemp.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int f;
-    float c;
-    printf("enter the temperature in Fahrenheit");
-    scanf("%d",&f);
-    c = ((f-32)*5)/9;
-    printf("%d is the temperature in celcius",c);
-}
