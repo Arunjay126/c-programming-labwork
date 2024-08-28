540c702ee7c6c26e62d85f0bdb7fe84bce005605
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:22:49 +0530

    Delete totalsec.c

--- totalsec.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int hour,min,sec,total;
-    printf("enter hour,min,sec");
-    scanf("%d%d%d",&hour,&min,&sec);
-    total = hour*3600 + min*60+ sec;
-    printf("total is %d",total);
-}
