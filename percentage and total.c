efe3b72738ff32a84fad0ad6df97fa65097a367c
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:21:40 +0530

    Delete percentage and total.c

--- percentage and total.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b,c,d,e,t,p;
-    printf("enter marks of 5 subjects");
-    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-    t = a+b+c+d+e;
-    p = (t*100)/500;
-    printf("%d is the total ammount \n %d is the percentage",t,p);
-}
