926c731d3303aad8612fb1819702469697713ad2
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:21:30 +0530

    Delete gross salary.c

--- gross salary.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int bs,hra,oa,gs;
-    printf("enter the basic salary, HRA, other allowence");
-    scanf("%d%d%d",&bs,&hra,&oa);
-    gs = bs+hra+oa;
-    printf("%d is the gross salary",gs);
-}
