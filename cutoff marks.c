c5d81649ca8121cd01d3e0219639d2729b695a2c
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:20:55 +0530

    Delete cutoff marks.c

--- cutoff marks.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int m,p,c,e,cm;
-    printf("enter maths(out of 200),physics(out of 200),chemistry(out of 200),entrance examination(out of 100)");
-    scanf("%d%d%d%d",&m,&p,&c,&e);
-    cm = m/2+p/2+c/2+e;
-    printf("cm is %d",cm);
-}
