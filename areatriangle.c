f538012f92e12a431b9d1958e10a5f9d356cc71c
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-08-28 10:20:42 +0530

    Delete areatriangle.c

--- areatriangle.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
-    printf("enter the height and base of a triangle");
-    scanf("%d%d",&a,&b);
-    c = a*b/2;
-    printf("%d is the area of triangle",c);
-}
