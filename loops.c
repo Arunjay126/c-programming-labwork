d6ffc575af47348eb798cac18e62bbcae4a3c4f9
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-09-04 09:09:40 +0530

    Create loops.c

---
+++ loops.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    int a = 1;
+    int i = 5;
+    while(a<=10){
+        int b;
+        b = i * a;
+        printf("%d\n",b);
+        a++;
+    }
+}
+
+#include <stdio.h>
+int main(){
+    int a = 1;
+    int i = 5;
+    do{
+        printf("%d \n",a*5);
+        a++;
+    }
+    while(a<=10);
+}
+
+#include<stdio.h>
+int main(){
+    int a = 5;
+    int x=0;
+    for(x = 1;x<=10;x++){
+        printf("%d\n",x*a);
+    }
+}
