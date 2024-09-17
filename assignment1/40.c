507f81d7146328066f8511b91693eca1051f0842
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-09-18 00:58:45 +0530

    Create 40.c

---
+++ assignment1/40.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main(){
+    int a = 0,b= 0;
+    while(a>=0){
+        printf("enter the value: ");
+        b+=a;
+        scanf("%d",&a);
+    }
+    printf("%d",b);
+    
+}
