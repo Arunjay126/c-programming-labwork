3e22a2fe2b353cf5cd474c18d3a0cbf5726b1c98
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-09-18 09:37:31 +0530

    Create 2.c

---
+++ pattern_worksheet/2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a = 4;
+    for(int x = 1;x<=a;x++){
+        for(int y = 1;y<=x;y++){
+            printf("%d ",x);
+        }
+        printf("\n");
+    }
+}
