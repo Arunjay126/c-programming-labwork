33b59b25bb8f94ce108ac24850ffc6bb3e0c1158
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-09-18 09:59:49 +0530

    Create 8.c

---
+++ pattern_worksheet/8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    for(int x = 4;x<=4;x--){
+        for(int y = 1;y<=x;y++){
+            printf("%d ",y);
+            
+        }
+        printf("\n");
+    }
+}
