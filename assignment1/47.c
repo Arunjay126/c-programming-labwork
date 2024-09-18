b0d9f14eead3598a09d29ea63cddcd52175267ae
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-09-18 08:52:18 +0530

    Update 47.c

--- assignment1/47.c
+++ assignment1/47.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 int main(){
-    int a;
-    printf("enter the number of rows: ");
-    scanf("%d",&a);
+    int a = 5;
     for (int x = 1; x<=a;x++){
         for (int y = 0;y<x;y++){
             printf("%c",'A'+y);
