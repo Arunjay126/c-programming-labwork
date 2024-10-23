69436cf2249a7cd5ae4dd298cdfd5bfb10d4a07f
ACRON <72449359+Arunjay126@users.noreply.github.com>
2024-10-23 09:56:24 +0530

    Update 34.c

--- assignment1/34.c
+++ assignment1/34.c
@@ -3,7 +3,7 @@ int main(){
     int a,x = 0,y = 1;
     printf("enter the number of times you want to have series: ");
     scanf("%d",&a);
-    for (int z = 1;z<=a;z++){
+    for (int z = 1;z<=a/2+1;z++){
         printf("%d %d ",x,y);
         x = x+y;
         y = y+x;
